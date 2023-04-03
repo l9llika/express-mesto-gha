acfae7ccf71cdf3b8cf313e9ee4a88cc25c61434
Ruslan Rzaev <69870105+l9llika@users.noreply.github.com>
2023-04-03 13:43:44 +0700

    fix for tests

--- controllers/users.js
+++ controllers/users.js
@@ -16,11 +16,6 @@ module.exports.getUserById = (req, res) => {
       return res.status(200).send({ user });
     })
     .catch((err) => {
-      // if (err.name === 'ValidationError') {
-      //   return res
-      //     .status(INCORRECT_ERROR_CODE)
-      //     .send({ message: 'Переданы некорректные данные пользователя' });
-      // }
       if (err.name === 'CastError') {
         return res
           .status(INCORRECT_ERROR_CODE)
@@ -58,10 +53,6 @@ module.exports.updateUser = (req, res) => {
           .status(INCORRECT_ERROR_CODE)
           .send({ message: 'Переданы некорректные данные при обновлении профиля' });
       }
-      // if (err.name === 'CastError') {
-      // eslint-disable-next-line max-len
-      //   return res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Пользователь по указанному _id не найден' });
-      // }
       return res.status(DEFAULT_ERROR_CODE).send({ message: 'Ошибка по умолчанию' });
     });
 };
@@ -76,10 +67,6 @@ module.exports.updateAvatar = (req, res) => {
           .status(INCORRECT_ERROR_CODE)
           .send({ message: 'Переданы некорректные данные при обновлении аватара' });
       }
-      // if (err.name === 'CastError') {
-      // eslint-disable-next-line max-len
-      //   return res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Пользователь по указанному _id не найден' });
-      // }
       return res.status(DEFAULT_ERROR_CODE).send({ message: 'Ошибка по умолчанию' });
     });
 };
